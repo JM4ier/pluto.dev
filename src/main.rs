6a1512662884fcca1e34d4f5da18b039d0524e78
Jonas Maier <1.jmaier.3@gmail.com>
2021-04-03 15:32:53 +0200

    finalize full transition to db

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use pulldown_cmark::*;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
-use walkdir::WalkDir;
 
 mod code;
 mod models;
+mod render;
 mod schema;
 
 use models::Post;
@@ -23,190 +22,6 @@ pub fn establish_connection() -> PgConnection {
     dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
     PgConnection::establish(&db_url).expect(&format!("Error connecting to {}", db_url))
-}
-
-fn files(path: &str) -> Vec<String> {
-    WalkDir::new(path)
-        .into_iter()
-        .filter_map(Result::ok)
-        .filter(|e| e.metadata().map_or(false, |m| m.is_file()))
-        .map(|e| e.path().to_string_lossy().into())
-        .collect()
-}
-
-fn to_html(meta: &Post, db: &PgConnection) -> AResult<String> {
-    let markdown = &meta.content;
-    let parser = Parser::new(markdown);
-
-    let mut code_lang = None;
-
-    use Event::*;
-    let parser = parser.flat_map(|event| match &event {
-        Start(Tag::CodeBlock(kind)) => {
-            if let CodeBlockKind::Fenced(kind) = kind {
-                code_lang = Some(kind.clone().into_string());
-            }
-            vec![Html(r#"<div class="code">"#.into()), event]
-        }
-        Text(code) => code_lang
-            .as_ref()
-            .map(|lang| code::highlight(&code, &lang).ok())
-            .flatten()
-            .map_or(vec![event], |html| vec![Html(html.into())]),
-        End(Tag::CodeBlock(_)) => {
-            code_lang = None;
-            vec![event, Html("</div>".into())]
-        }
-        _ => {
-            vec![event]
-        }
-    });
-
-    let mut html_out = String::new();
-    html::push_html(&mut html_out, parser);
-
-    Ok(format!(
-        include_str!("skeleton.html"),
-        body = html_out,
-        title = meta.title,
-        copyright = copyright_years(&meta.created, &meta.updated),
-        bottom_navigation = bottom_navigation(meta, db)?,
-    ))
-}
-
-use chrono::NaiveDateTime;
-fn copyright_years(from: &NaiveDateTime, to: &NaiveDateTime) -> String {
-    let mut copyright = format!("{}", from.date().format("%Y"));
-    let to = format!("{}", to.date().format("%Y"));
-    if copyright != to {
-        copyright = format!("{}-{}", copyright, to);
-    }
-    copyright
-}
-
-fn bottom_navigation(this: &Post, db: &PgConnection) -> AResult<String> {
-    use crate::schema::posts::dsl::*;
-
-    let prev = posts
-        .filter(created.lt(&this.created).and(published))
-        .order_by(created.desc())
-        .limit(1)
-        .load::<Post>(db)?;
-
-    let next = posts
-        .filter(created.gt(&this.created).and(published))
-        .order_by(created.asc())
-        .limit(1)
-        .load::<Post>(db)?;
-
-    let first = posts
-        .filter(published)
-        .order_by(created.asc())
-        .limit(1)
-        .load::<Post>(db)?;
-
-    let last = posts
-        .filter(published)
-        .order_by(created.desc())
-        .limit(1)
-        .load::<Post>(db)?;
-
-    let link = |dir, linked: &Post| {
-        format!(
-            r#" <a href="{}" class="bottom-nav-button">{}</a> "#,
-            linked.url, dir
-        )
-    };
-
-    let (lname, llink) = match prev.first() {
-        Some(first) => ("Prev", first),
-        None => ("Last", last.first().unwrap()),
-    };
-
-    let (rname, rlink) = match next.first() {
-        Some(first) => ("Next", first),
-        None => ("First", first.first().unwrap()),
-    };
-
-    let mut links = String::new();
-
-    links += &link(format!("← {}", lname), llink);
-    links += &link(format!("{} →", rname), rlink);
-
-    Ok(links)
-}
-
-fn render_overview(db: &PgConnection) -> AResult<()> {
-    let mut body = String::from("<h1>Blog Posts</h1>");
-    body += "<hr>";
-
-    use crate::schema::posts::dsl::*;
-    let sites = posts
-        .filter(published)
-        .order_by(created.desc())
-        .load::<Post>(db)?;
-
-    body += r#"<table class="post-list">"#;
-    body += "<th>Post</th><th>Date</th>";
-    for site in sites.iter() {
-        body += &format!(
-            r#"<tr><td><a href="{}">{}</a></td><td>{}</td></tr>"#,
-            site.url,
-            site.title,
-            site.created.date().format("%d-%m-%Y")
-        );
-    }
-    body += "</table>";
-
-    let page = format!(
-        include_str!("skeleton.html"),
-        title = "Overview",
-        body = body,
-        bottom_navigation = "",
-        copyright = copyright_years(
-            &sites.last().unwrap().created,
-            &sites.first().unwrap().created
-        )
-    );
-
-    std::fs::write("html/index.html", page)?;
-
-    Ok(())
-}
-
-fn process(file: &str, db: &PgConnection, force_render: bool) -> AResult<()> {
-    use crate::schema::posts::dsl::*;
-    use diesel::dsl::*;
-
-    let mut meta = read_md(file)?;
-    meta.url = file.into();
-
-    let existing = posts.filter(url.eq(&meta.url)).limit(1).load::<Post>(db)?;
-
-    if let Some(entry) = existing.first() {
-        meta.updated = meta.created;
-        meta.created = entry.created;
-
-        if meta.version == entry.version {
-            if force_render && meta.published {
-                //render(&meta, db)?;
-            }
-            return Ok(());
-        }
-    }
-
-    delete(posts.filter(url.eq(&meta.url))).execute(db)?;
-    insert_into(schema::posts::table)
-        .values(&meta)
-        .execute(db)?;
-
-    println!("inserting or updating `{}`.", meta.url);
-
-    if meta.published {
-        //render(&meta, db)?;
-    }
-
-    Ok(())
 }
 
 fn read_md(path: &str) -> AResult<Post> {
@@ -296,6 +111,19 @@ fn list(filter: &str, db: &PgConnection) -> AResult<()> {
 }
 
 fn render_all(db: &PgConnection) -> AResult<()> {
+    use crate::schema::posts::dsl::*;
+    let pages = posts.filter(published).load::<Post>(db)?;
+
+    for page in pages.iter() {
+        println!("rendering {}.", page.url);
+        let rendered = render::blogpost(page, db)?;
+        std::fs::write(format!("html/{}", page.url), rendered)?;
+    }
+
+    println!("rendering overview.");
+    let overview = render::overview(db)?;
+    std::fs::write("html/index.html", overview)?;
+
     Ok(())
 }
 
@@ -332,10 +160,10 @@ fn main() -> AResult<()> {
 
     if let Some(post) = matches.value_of("edit") {
         edit(post, &connection)?;
-    } else if let Some(filter) = matches.value_of("list") {
-        list(filter, &connection)?;
     } else if matches.is_present("render") {
         render_all(&connection)?;
+    } else if let Some(filter) = matches.value_of("list") {
+        list(filter, &connection)?;
     }
     Ok(())
 }
---
+++ src/render.rs
@@ -0,0 +1,144 @@
+use super::*;
+
+fn render_markdown(post: &str) -> String {
+    use pulldown_cmark::*;
+    let parser = Parser::new(post);
+
+    let mut code_lang = None;
+
+    use Event::*;
+    let parser = parser.flat_map(|event| match &event {
+        Start(Tag::CodeBlock(kind)) => {
+            if let CodeBlockKind::Fenced(kind) = kind {
+                code_lang = Some(kind.clone().into_string());
+            }
+            vec![Html(r#"<div class="code">"#.into()), event]
+        }
+        Text(code) => code_lang
+            .as_ref()
+            .map(|lang| code::highlight(&code, &lang).ok())
+            .flatten()
+            .map_or(vec![event], |html| vec![Html(html.into())]),
+        End(Tag::CodeBlock(_)) => {
+            code_lang = None;
+            vec![event, Html("</div>".into())]
+        }
+        _ => {
+            vec![event]
+        }
+    });
+
+    let mut html_out = String::new();
+    html::push_html(&mut html_out, parser);
+
+    html_out
+}
+
+use chrono::NaiveDateTime;
+fn copyright_years(from: &NaiveDateTime, to: &NaiveDateTime) -> String {
+    let mut copyright = format!("{}", from.date().format("%Y"));
+    let to = format!("{}", to.date().format("%Y"));
+    if copyright != to {
+        copyright = format!("{}-{}", copyright, to);
+    }
+    copyright
+}
+
+fn bottom_navigation(this: &Post, db: &PgConnection) -> AResult<String> {
+    use crate::schema::posts::dsl::*;
+
+    let prev = posts
+        .filter(created.lt(&this.created).and(published))
+        .order_by(created.desc())
+        .limit(1)
+        .load::<Post>(db)?;
+
+    let next = posts
+        .filter(created.gt(&this.created).and(published))
+        .order_by(created.asc())
+        .limit(1)
+        .load::<Post>(db)?;
+
+    let first = posts
+        .filter(published)
+        .order_by(created.asc())
+        .limit(1)
+        .load::<Post>(db)?;
+
+    let last = posts
+        .filter(published)
+        .order_by(created.desc())
+        .limit(1)
+        .load::<Post>(db)?;
+
+    let link = |dir, linked: &Post| {
+        format!(
+            r#" <a href="{}" class="bottom-nav-button">{}</a> "#,
+            linked.url, dir
+        )
+    };
+
+    let (lname, llink) = match prev.first() {
+        Some(first) => ("Prev", first),
+        None => ("Last", last.first().unwrap()),
+    };
+
+    let (rname, rlink) = match next.first() {
+        Some(first) => ("Next", first),
+        None => ("First", first.first().unwrap()),
+    };
+
+    let mut links = String::new();
+
+    links += &link(format!("← {}", lname), llink);
+    links += &link(format!("{} →", rname), rlink);
+
+    Ok(links)
+}
+
+pub fn blogpost(post: &Post, db: &PgConnection) -> AResult<String> {
+    let html = render_markdown(&post.content);
+    Ok(format!(
+        include_str!("skeleton.html"),
+        body = html,
+        title = post.title,
+        copyright = copyright_years(&post.created, &post.updated),
+        bottom_navigation = bottom_navigation(post, db)?,
+    ))
+}
+
+pub fn overview(db: &PgConnection) -> AResult<String> {
+    let mut body = String::from("<h1>Blog Posts</h1>");
+    body += "<hr>";
+
+    use crate::schema::posts::dsl::*;
+    let sites = posts
+        .filter(published)
+        .order_by(created.desc())
+        .load::<Post>(db)?;
+
+    body += r#"<table class="post-list">"#;
+    body += "<th>Post</th><th>Date</th>";
+    for site in sites.iter() {
+        body += &format!(
+            r#"<tr><td><a href="{}">{}</a></td><td>{}</td></tr>"#,
+            site.url,
+            site.title,
+            site.created.date().format("%d-%m-%Y")
+        );
+    }
+    body += "</table>";
+
+    let page = format!(
+        include_str!("skeleton.html"),
+        title = "Overview",
+        body = body,
+        bottom_navigation = "",
+        copyright = copyright_years(
+            &sites.last().unwrap().created,
+            &sites.first().unwrap().created
+        )
+    );
+
+    Ok(page)
+}
